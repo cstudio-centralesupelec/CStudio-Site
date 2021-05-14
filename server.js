fbd8d6306145005415b9292babca3971dec06f27
vanyle <delegue.antoine@gmail.com>
2021-05-15 01:34:41 +0200

    Added HTTPS support

--- server.js
+++ server.js
@@ -27,6 +27,29 @@ app.use(express.static('static'));
 require('./server/routes.js'); // setup routes
 app.use(vw.api());
 
-app.listen(config.port || 80,config.host || "0.0.0.0",() => {
+let server = null;
+
+if(config.debug){
+	// full https security.
+	let redirection_server = http.createServer((req,res) => {
+		response.writeHead(302, {
+		  'Location': `https://${config.hostname}:${config.port}${req.url}`
+		});
+		response.end();
+	});
+
+	const options = {
+		key: fs.readFileSync('cert/privkey.pem'),
+		cert: fs.readFileSync('cert/cert.pem')
+	};
+	server = https.createServer(options,app);
+	redirection_server.listen(80, config.host || "0.0.0.0");
+
+}else{
+	// http debug server.
+	server = http.createServer(app);
+}
+
+server.listen(config.port || 443,config.host || "0.0.0.0",() => {
 	console.log(`Website available at http://${config.hostname}:${config.port}/`)
 });
